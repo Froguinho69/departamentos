01f961d3f9c7a89fa8c64ce6e76e7a2e74fb4aef
unknown <23114290059@ACADEMICO.IESB.BR>
2024-05-16 21:55:14 -0300

    first commit

---
+++ src/controllers/FuncionarioController.js
@@ -0,0 +1,62 @@
+const Funcionario = require('../models/Funcionario')
+
+async function create(req, res) {
+    try {
+        const funcionario = new Funcionario(req.body)
+        const funcionarioCriado = await funcionario.save()
+        res.status(201).json(funcionarioCriado)
+    } 
+    catch (error) {
+        console.error("Erro ao criar funcionario: ", error)
+        res.status(400).json(
+            {
+                mensagem: "Erro ao criar funcionario!",
+                erro: error.message
+            }
+        )
+    }
+}
+
+async function getById(req, res) {
+    const funcionario = await Funcionario.findById(req.params.id).populate('cargo')
+    if (funcionario) {
+        res.json(funcionario)
+    } 
+    else {
+        res.status(404).json({ mensagem: "Funcionario não encontrato!" })
+    }
+}
+
+
+async function getAll(req, res) {
+    res.json(await Funcionario.find())
+}
+
+
+async function update(req, res) {
+    try {
+        const funcionarioAtualizado = await Funcionario.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.json(funcionarioAtualizado)
+    }
+    catch (error) {
+        console.log("Erro ao criar funcionario: ", error)
+        res.status(400).json({
+            mensagem: "Erro ao Atualizar funcionario!",
+            erro: error.mensage
+        })
+    }
+}
+
+async function remove(req, res) {
+    await Funcionario.findByIdAndDelete(req.params.id)
+    res.json({mensagem: "Excluido com sucesso!"})
+}
+
+module.exports = {
+    create,
+    getById,
+    getAll,
+    update,
+    remove
+
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+// Controllers
+const CargoController = require('../controllers/CargoController')
+const FuncionarioController = require('../controllers/FuncionarioController')
+const DepartamentoController = require('../controllers/DepartamentoController')
+
+// validators
+const { validarId } = require('../validators/IdValidator')
+const { cargoValidador } = require('../validators/CargoValidator')
+const { departamentoValidador } = require('../validators/DepartamentoValidator');
+
+// Cargos
+router.post('/cargos', cargoValidador, CargoController.create)
+router.get('/cargos', CargoController.getAll)
+router.get('/cargos/:id', validarId, CargoController.getById)
+router.put('/cargos/:id', validarId, cargoValidador, CargoController.update)
+router.delete('/cargos/:id', validarId, CargoController.remove)
+
+// Funcionarios
+router.post('/funcionarios', FuncionarioController.create)
+router.get('/funcionarios/:id', FuncionarioController.getById)
+router.get('/funcionarios', FuncionarioController.getAll)
+router.put('/funcionarios/:id', FuncionarioController.update)
+router.delete('/funcionarios/:id', FuncionarioController.remove)
+
+// Departamentos
+router.post('/departamentos', departamentoValidador, DepartamentoController.create);
+router.get('/departamentos', DepartamentoController.getAll);
+router.get('/departamentos/:id', validarId, DepartamentoController.getById);
+router.put('/departamentos/:id', validarId, departamentoValidador, DepartamentoController.update);
+router.delete('/departamentos/:id', validarId, DepartamentoController.remove);
+
+module.exports = router
